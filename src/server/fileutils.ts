d9f4d5ca074b4f058c904179943edf77d5d61a94
Anton Kiselev <anton.kisel@gmail.com>
2021-04-26 02:27:14 +0700

    thumbnail support

--- src/server/fileutils.ts
+++ src/server/fileutils.ts
@@ -1,4 +1,5 @@
 import * as fse from 'fs-extra'
+import _ = require('lodash');
 import * as path from 'path';
 
 export const RE_SAFE_FILENAME = /^[-a-zA-Z0-9_]+$/
@@ -40,4 +41,48 @@ export async function getSubdirNames(dir: string): Promise<string[]> {
 
 export async function getDirFilenames(dir: string, file_regexp: RegExp): Promise<string[]> {
     return readDirNames(dir, (fullpath, fstat) => file_regexp.test(fullpath) && fstat.isFile());
+}
+
+export interface FileInfo {
+    name: string;
+    parentPath: string;
+    fullpath: string;
+    fstat: fse.Stats;
+}
+
+async function readDirStat(dir: string): Promise<FileInfo[]> {
+    const filenames = await fse.readdir(dir);
+    const parentPath = dir;
+    return Promise.all(filenames.map(name => {
+        const fullpath = path.join(dir, name);
+        return fse.stat(fullpath).catch(() => null).then( fstat => ({
+            name, fullpath, fstat, parentPath,
+        }));
+    })).then(res => res.filter(fi => fi.fstat != null));
+}
+
+function sortNewestFirst(a: FileInfo, b: FileInfo): number {
+    return b.fstat.mtimeMs - a.fstat.mtimeMs;
+}
+
+type FileInfoFilter = (fi: FileInfo) => boolean;
+// find latest file at exact depth
+export async function findNewestFileDeep(dir: string, depth: number, fileFilter: FileInfoFilter): Promise<FileInfo> {
+    if (depth < 0) {
+        return null;
+    }
+    let items = await readDirStat(dir);
+    items.sort(sortNewestFirst);
+    for (const fi of items) {
+        if (depth == 0 && fi.fstat.isFile() && fileFilter(fi)) {
+            return fi;
+        }
+        if (depth > 0 && fi.fstat.isDirectory()) {
+            const res = await findNewestFileDeep(path.join(dir, fi.name), depth - 1, fileFilter);
+            if (res != null) {
+                return res;
+            }
+        }
+    }
+    return null;
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -4,7 +4,7 @@ import * as express from 'express';
 import {current_config} from './config'
 import { ListResponse, ListItem } from '../common/models';
 import { apiError, apiWrapper, errorWrapper } from './utils';
-import { getDirFilenames, getSubdirNames, verifySafeFileName } from './fileutils';
+import { FileInfo, findNewestFileDeep, getDirFilenames, getSubdirNames, verifySafeFileName } from './fileutils';
 import tmp = require('tmp');
 import { convertToMp4, getVideoThumbnail } from './ffmpeg';
 import { apiFileGenWrapper } from './tmpfileproc';
@@ -46,7 +46,7 @@ router.get('/api/list/:camname/:date/:hour', apiWrapper<ListResponse>(async req 
     }
 }));
 
-router.get('/api/list/:camname/:date/:hour/:basename.mp4', errorWrapper(async (req, res) => {
+router.get('/api/mp4/:camname/:date/:hour/:basename.mp4', errorWrapper(async (req, res) => {
     const camname = verifySafeFileName(req.params.camname);
     const date = verifySafeFileName(req.params.date);
     const hour = verifySafeFileName(req.params.hour);
@@ -60,7 +60,13 @@ router.get('/api/list/:camname/:date/:hour/:basename.mp4', errorWrapper(async (r
     });
 }));
 
-router.get('/api/list/:camname/:date/:hour/:basename.jpg', errorWrapper(async (req, res) => {
+function getStoragePathFromParams(req: express.Request, keys: string[]): string {
+    const paramPath = keys.map(k => verifySafeFileName(req.params[k]));
+    const resPath = path.join(current_config.storage, ...paramPath);
+    return resPath;
+}
+
+router.get('/api/thumbnail/:camname/:date/:hour/:basename.jpg', errorWrapper(async (req, res) => {
     const camname = verifySafeFileName(req.params.camname);
     const date = verifySafeFileName(req.params.date);
     const hour = verifySafeFileName(req.params.hour);
@@ -73,6 +79,50 @@ router.get('/api/list/:camname/:date/:hour/:basename.jpg', errorWrapper(async (r
         return tmpFile;
     });
 }));
+
+function tsFileFilter(fi: FileInfo) {
+    return /.*[.]ts$/.test(fi.name);
+}
+
+const fullPathLen = ['camname', 'date', 'hour'].length;
+async function deepThumbHandler(req: express.Request, res: express.Response, paramKeys: string[]) {
+    const relPath = paramKeys.map(k => verifySafeFileName(req.params[k]));
+    const searchdir = path.join(current_config.storage, ...relPath);
+    const fi = await findNewestFileDeep(searchdir, fullPathLen - paramKeys.length, tsFileFilter);
+    console.log("found last:", fi.fullpath)
+
+    await apiFileGenWrapper(req, res, async (tmpDir)=> {
+        const tmpFile = path.join(tmpDir, `thumbnail.jpg`);
+        await getVideoThumbnail(fi.fullpath, tmpFile)
+        return tmpFile;
+    });
+}
+router.get('/api/thumbnail/:camname/:date/:hour/', errorWrapper(async (req, res) => {
+    await deepThumbHandler(req, res, ['camname', 'date', 'hour'])
+}));
+
+router.get('/api/thumbnail/:camname/:date/', errorWrapper(async (req, res) => {
+    await deepThumbHandler(req, res, ['camname', 'date'])
+}));
+
+router.get('/api/thumbnail/:camname/', errorWrapper(async (req, res) => {
+    await deepThumbHandler(req, res, ['camname'])
+}));
+
+// router.get('/api/thumbnail/:camname/:date/:hour/', errorWrapper(async (req, res) => {
+//     const camname = verifySafeFileName(req.params.camname);
+//     const date = verifySafeFileName(req.params.date);
+//     const hour = verifySafeFileName(req.params.hour);
+//     const searchdir = path.join(current_config.storage, camname, date, hour);
+//     const fi = await findNewestFileDeep(searchdir, 0, tsFileFilter);
+
+//     await apiFileGenWrapper(req, res, async (tmpDir)=> {
+//         const tmpFile = path.join(tmpDir, `thumbnail.jpg`);
+//         await getVideoThumbnail(fi.fullpath, tmpFile)
+//         return tmpFile;
+//     });
+// }));
+
 
 
 router.all('/api/*', (req, res) => {
