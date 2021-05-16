4f2148457f22d20bfd7050fc747415402a31fe58
Anton Kiselev <anton.kisel@gmail.com>
2021-05-16 23:53:33 +0700

    add video.js, update bootstrap

---
+++ src/common/videoplayer.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import videojs, { VideoJsPlayerOptions } from 'video.js'
+import "video.js/dist/video-js.css"
+
+
+// see https://docs.videojs.com/tutorial-react.html
+export default class VideoPlayer extends React.Component<VideoJsPlayerOptions> {
+  player: any;
+  videoNode: any;
+
+  componentDidMount() {
+    this.player = videojs(this.videoNode, this.props, () => {
+      console.log('onPlayerReady', this)
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose()
+    }
+  }
+
+  // wrap the player in a div with a `data-vjs-player` attribute
+  // so videojs won't create additional wrapper in the DOM
+  // see https://github.com/videojs/video.js/pull/3856
+  render() {
+    return (
+      <div>
+        <div data-vjs-player>
+          <video ref={node => this.videoNode = node} className="video-js"></video>
+        </div>
+      </div>
+    )
+  }
+}
--- src/web/components/cam_player.tsx
+++ src/web/components/cam_player.tsx
@@ -4,8 +4,31 @@ import { observer } from "mobx-react-lite";
 import _ = require("lodash");
 import { theLocation } from "../store/location";
 import { theSettingsStore } from "../store/settings";
+import VideoPlayer from "../../common/videoplayer";
 
 export const CamVideoPlayer = observer(() => {
+    const absLoc = theLocation.path.split('/').slice(2, -1); // strip /view/ and /$
+    const videoPath = absLoc.join('/');
+    const vformat = (theSettingsStore.legacyMode) ? 'mp4-legacy' : 'mp4'
+    const videoURL = `/api/video/${vformat}/${videoPath}`
+    return (
+        <div className="video-player">
+            <VideoPlayer {...{
+                autoplay: true,
+                controls: true,
+                responsive: true,
+                fluid: true,
+                playbackRates: [1, 2, 5, 10, 20, 30],
+                sources: [
+                    {src: videoURL, type: 'video/mp4'}
+                ]
+            }} />
+        </div>
+    );
+});
+
+
+export const CamVideoPlayerLegacy = observer(() => {
     const absLoc = theLocation.path.split('/').slice(2, -1); // strip /view/ and /$
     const videoPath = absLoc.join('/');
     const vformat = (theSettingsStore.legacyMode) ? 'mp4-legacy' : 'mp4'
