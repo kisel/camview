79a2d2ec50f641229f99c54b8d9b19ee0962cb0d
Anton Kiselev <anton.kisel@gmail.com>
2021-08-23 20:53:17 +0700

    add draft motion detector

---
+++ motion-detector/motion_detector.py
@@ -0,0 +1,147 @@
+#!/bin/env python
+
+import cv2
+import json
+import time
+import os
+import sys
+import argparse
+import numpy as np
+
+GREEN = (0, 255, 0)
+log = lambda msg: None
+
+def process_input(streamSrc, args):
+    ref_frame = None
+    video = cv2.VideoCapture(streamSrc)
+
+    frameIdx = 0
+    height, width = 0, 0
+    minArea = 0
+    move_seq_len = 0
+    base_resolution = [800, 600]
+    motion_frames = 0
+    too_many_objects = 0
+
+    while True:
+        check, frame = video.read()
+        if not check:
+            log("No frames to read")
+            break
+        frame = cv2.resize(frame, base_resolution)
+        frameIdx += 1
+        if frameIdx == 1:
+            height, width = frame.shape[:2]
+            minArea = width * height / args.factor
+            if args.gui:
+                log(f"Resol {width}x{height}")
+                log(f"minArea={minArea}")
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+        if ref_frame is None:
+            ref_frame = gray
+            continue
+
+        delta_frame = cv2.absdiff(ref_frame,gray)
+
+        if frameIdx % args.ref_frames == 0:
+            ref_frame = gray
+        thres_frame = cv2.threshold(delta_frame, 80, 255, cv2.THRESH_BINARY)[1]
+        thres_frame = cv2.dilate(thres_frame, None, iterations=2)
+
+        cnts, _ = cv2.findContours(thres_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        moved_objects = [c for c in cnts if cv2.contourArea(c) > minArea]
+        if len(moved_objects) > args.max_groups:
+            too_many_objects += 1
+            move_seq_len = 0
+        elif len(moved_objects) > 0:
+            move_seq_len += 1
+        else:
+            move_seq_len = 0
+
+        if move_seq_len > args.min_seq:
+            motion_frames += 1
+
+        if args.gui:
+            if move_seq_len > args.min_seq:
+                for contour in moved_objects:
+                    (x, y, w, h) = cv2.boundingRect(contour)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), GREEN, 3)
+            if not args.one_window:
+                cv2.imshow("Color Frame", frame)
+                cv2.imshow("Gray Frame", gray)
+                cv2.imshow("Delta Frame", delta_frame)
+                cv2.imshow("Threshold Frame", thres_frame)
+            else:
+                sidebar = np.vstack((gray, delta_frame, thres_frame))
+                h, w = frame.shape[:2]
+                quad = np.hstack((
+                    frame,
+                    g2c(cv2.resize(sidebar, [int(w/3), h]))
+                    ))
+                cv2.imshow("Quad", quad)
+            key = cv2.waitKey(1)
+            time.sleep(1.0 / args.fps)
+    video.release()
+    if args.gui:
+        cv2.destroyAllWindows()
+    return {
+            'stream': streamSrc,
+            'motion_frames_total': motion_frames,
+            'too_many_objects': too_many_objects
+            }
+
+def g2c(g):
+    return cv2.cvtColor(g, cv2.COLOR_GRAY2BGR)
+
+def main():
+    parser = argparse.ArgumentParser(description='Motion detector')
+    parser.add_argument('--gui', action='store_true')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-W', '--one-window', action='store_true')
+    parser.add_argument('--fps', default=60, type=int, help='draw fps')
+    parser.add_argument('-o', '--output',
+            help='output stats json filename' )
+    parser.add_argument('-r', '--ref-frames', type=int, default=1,
+            help='update reference frame each "r" frames' )
+    parser.add_argument('-f', '--factor', type=int, default=1000,
+            help='area/factor - minimal area treated as change' )
+    parser.add_argument('--max-groups', type=int, default=10,
+            help='ignore false detections of too many objects due cam reinit' )
+    parser.add_argument('--threads', type=int,
+            help='cv2.setNumThreads. 0 - no threads')
+    parser.add_argument('--min-seq', type=int, default=3,
+            help='minimal sequence of changed frames to capture movement' )
+    parser.add_argument('input', nargs='+', help="file/rtsp stream")
+    args=parser.parse_args()
+
+    if type(args.threads) is int:
+        cv2.setNumThreads(args.threads)
+    if args.one_window:
+       args.gui = True
+    if args.gui or args.verbose:
+        log = lambda msg: print(msg)
+    detector_results = []
+    for streamSrc in args.input:
+        t1 = time.time()
+        res = process_input(streamSrc, args)
+        t2 = time.time()
+        res['process_time'] = t2 - t1
+        detector_results.append(res)
+        log(json.dumps(res, indent=2))
+    if args.output:
+        out = json.dumps({
+            'detector_results': detector_results,
+            'args': sys.argv,
+            }, indent=2)
+        if args.output == '-':
+            print(out)
+        else:
+            with open(args.output, 'w') as f:
+                f.write(out)
+
+if __name__ == '__main__':
+    main()
+
